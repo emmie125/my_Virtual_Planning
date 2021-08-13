92c0b03c0c6bb05ff66e9ba6acdc55a1aefa9d25
emmie125 <mulangaemmanuella@gmail.com>
2021-08-13 20:58:26 +0200

    feat: add methode of  hashing password with bcrypt

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,17 +1,51 @@
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 // eslint-disable-next-line import/extensions
 import UserModel from '../model/userModel.js';
 
-export const postUser = (req, res) => {
-  const user = new UserModel({
-    ...req.body,
+export const postUser = async (req, res) => {
+  const saltRounds = 10;
+  const {
+    username,
+    password,
+    imageProfil,
+    firstname,
+    lastname,
+    dateBirth,
+    gender,
+    phoneNumber,
+    address,
+    userType,
+    statusUser,
+    accountSession,
+    cDate,
+  } = req.body;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    console.log(hash);
+    const user = new UserModel({
+      username,
+      password: hash,
+      imageProfil,
+      firstname,
+      lastname,
+      dateBirth,
+      gender,
+      phoneNumber,
+      address,
+      userType,
+      statusUser,
+      accountSession,
+      cDate,
+    });
+    user
+      .save()
+      .then(() =>
+        res
+          .status(200)
+          .json({ message: 'Utilisateur enregistré avec succès !' })
+      )
+      .catch((error) => res.status(500).json(error.message));
   });
-  user
-    .save()
-    .then(() =>
-      res.status(200).json({ message: 'Utilisateur enregistré avec succès !' })
-    )
-    .catch((error) => res.status(500).json(error.message));
 };
 
 export const getUsers = (req, res) => {
--- src/model/userModel.js
+++ src/model/userModel.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
     password: { type: String, required: true },
-    image_profil: { type: String, required: true },
+    imageProfil: { type: String, required: true },
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    date_birth: { type: Date, required: true },
+    dateBirth: { type: Date, required: true },
     gender: { type: String, required: true },
-    phone_number: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
     address: {
       type: {
         avenue: { type: String },
@@ -18,12 +18,12 @@ const userSchema = new mongoose.Schema(
         ville: { type: String },
       },
     },
-    user_type: {
+    userType: {
       type: { id: { type: Number }, name: { type: String } },
     },
-    status_user: { type: Boolean },
-    account_session: { type: Boolean },
-    c_date: { type: Date },
+    statusUser: { type: Boolean },
+    accountSession: { type: Boolean },
+    cDate: { type: Date },
   },
   { timestamps: true }
 );
